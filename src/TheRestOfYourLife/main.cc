a22394a1e7deb554ec24227ff1aff6442b57ea8d
Chaitanya Kumar <chaitanyak28771@gmail.com>
2023-06-02 17:03:15 +0200

    Delete files not required for the blog

--- src/TheRestOfYourLife/main.cc
+++ src/TheRestOfYourLife/main.cc
@@ -31,161 +31,161 @@ using std::chrono::seconds;
 using std::chrono::duration_cast;
 
 color ray_color(
-				const ray& r,
-				const color& background,
-				const hittable& world,
-				shared_ptr<hittable> lights,
-				int depth
-				) {
-	hit_record rec;
+		const ray& r,
+		const color& background,
+		const hittable& world,
+		shared_ptr<hittable> lights,
+		int depth
+		) {
+    hit_record rec;
 
-	// If we've exceeded the ray bounce limit, no more light is gathered.
-	if (depth <= 0)
-		return color(0,0,0);
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+	return color(0,0,0);
 
-	// If the ray hits nothing, return the background color.
-	if (!world.hit(r, 0.001, infinity, rec))
-		return background;
+    // If the ray hits nothing, return the background color.
+    if (!world.hit(r, 0.001, infinity, rec))
+	return background;
 
-	scatter_record srec;
-	color emitted = rec.mat_ptr->emitted(r, rec, rec.u, rec.v, rec.p);
+    scatter_record srec;
+    color emitted = rec.mat_ptr->emitted(r, rec, rec.u, rec.v, rec.p);
 
-	if (!rec.mat_ptr->scatter(r, rec, srec))
-		return emitted;
+    if (!rec.mat_ptr->scatter(r, rec, srec))
+	return emitted;
 
-	if (srec.is_specular) {
-		return srec.attenuation
-			* ray_color(srec.specular_ray, background, world, lights, depth-1);
-	}
+    if (srec.is_specular) {
+	return srec.attenuation
+	    * ray_color(srec.specular_ray, background, world, lights, depth-1);
+    }
 
-	auto light_ptr = make_shared<hittable_pdf>(lights, rec.p);
-	mixture_pdf p(light_ptr, srec.pdf_ptr);
-	ray scattered = ray(rec.p, p.generate(), r.time());
-	auto pdf_val = p.value(scattered.direction());
+    auto light_ptr = make_shared<hittable_pdf>(lights, rec.p);
+    mixture_pdf p(light_ptr, srec.pdf_ptr);
+    ray scattered = ray(rec.p, p.generate(), r.time());
+    auto pdf_val = p.value(scattered.direction());
 
-	return emitted
-		+ srec.attenuation * rec.mat_ptr->scattering_pdf(r, rec, scattered)
-		* ray_color(scattered, background, world, lights, depth-1)
-		/ pdf_val;
+    return emitted
+	+ srec.attenuation * rec.mat_ptr->scattering_pdf(r, rec, scattered)
+	* ray_color(scattered, background, world, lights, depth-1)
+	/ pdf_val;
 }
 
 
 hittable_list cornell_box() {
-	hittable_list objects;
+    hittable_list objects;
 
-	auto red   = make_shared<lambertian>(color(.65, .05, .05));
-	auto white = make_shared<lambertian>(color(.73, .73, .73));
-	auto green = make_shared<lambertian>(color(.12, .45, .15));
-	auto light = make_shared<diffuse_light>(color(15, 15, 15));
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
 
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
-	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
-	objects.add(make_shared<flip_face>(make_shared<xz_rect>(213, 343, 227, 332, 554, light)));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
-	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
-	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<flip_face>(make_shared<xz_rect>(213, 343, 227, 332, 554, light)));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
-	shared_ptr<material> aluminum = make_shared<metal>(color(0.8, 0.85, 0.88), 0.0);
-	shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165,330,165), aluminum);
-	box1 = make_shared<rotate_y>(box1, 15);
-	box1 = make_shared<translate>(box1, vec3(265,0,295));
-	objects.add(box1);
+    shared_ptr<material> aluminum = make_shared<metal>(color(0.8, 0.85, 0.88), 0.0);
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165,330,165), aluminum);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
 
-	auto glass = make_shared<dielectric>(1.5);
-	objects.add(make_shared<sphere>(point3(190,90,190), 90 , glass));
+    auto glass = make_shared<dielectric>(1.5);
+    objects.add(make_shared<sphere>(point3(190,90,190), 90 , glass));
 
-	return objects;
+    return objects;
 }
 
 
 int main(int argc, char *argv[]) {
-	// Parse command line arguments
-	int image_width = 600, thread_count = 1;
+    // Parse command line arguments
+    int image_width = 600, thread_count = 1;
 
-	// Parse command line arguments
-	int opt = -1;
-	while ((opt = getopt(argc, argv, "w:t:")) != -1) {
-		switch (opt) {
-		case 'w':
-			if (optarg == nullptr) break;
-			image_width = std::atoi(optarg);
-			if (image_width <= 0) {
-				std::cerr << "Error: image width must be a positive integer, found " << optarg;
-				return 1;
-			}
-			break;
-		case 't':
-			if (optarg == nullptr) break;
-			thread_count = std::atoi(optarg);
-			if (thread_count <= 0) {
-				std::cerr << "Error: thread count must be a positive integer, found " << optarg;
-				return 1;
-			}
-			break;
-		default:
-			std::cerr << "Error: Invalid flag" << std::endl;
-			return 1;
-		}
+    // Parse command line arguments
+    int opt = -1;
+    while ((opt = getopt(argc, argv, "w:t:")) != -1) {
+	switch (opt) {
+	case 'w':
+	    if (optarg == nullptr) break;
+	    image_width = std::atoi(optarg);
+	    if (image_width <= 0) {
+		std::cerr << "Error: image width must be a positive integer, found " << optarg;
+		return 1;
+	    }
+	    break;
+	case 't':
+	    if (optarg == nullptr) break;
+	    thread_count = std::atoi(optarg);
+	    if (thread_count <= 0) {
+		std::cerr << "Error: thread count must be a positive integer, found " << optarg;
+		return 1;
+	    }
+	    break;
+	default:
+	    std::cerr << "Error: Invalid flag" << std::endl;
+	    return 1;
 	}
+    }
 
-	omp_set_num_threads(thread_count);
-	// Image
+    omp_set_num_threads(thread_count);
+    // Image
 
-	const auto aspect_ratio = 1.0 / 1.0;
-	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 100;
-	const int max_depth = 50;
+    const auto aspect_ratio = 1.0 / 1.0;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
-	// World
+    // World
   
-	auto lights = make_shared<hittable_list>();
-	lights->add(make_shared<xz_rect>(213, 343, 227, 332, 554, shared_ptr<material>()));
-	lights->add(make_shared<sphere>(point3(190, 90, 190), 90, shared_ptr<material>()));
+    auto lights = make_shared<hittable_list>();
+    lights->add(make_shared<xz_rect>(213, 343, 227, 332, 554, shared_ptr<material>()));
+    lights->add(make_shared<sphere>(point3(190, 90, 190), 90, shared_ptr<material>()));
 
-	auto world = cornell_box();
+    auto world = cornell_box();
 
-	color background(0,0,0);
+    color background(0,0,0);
 
-	// Camera
+    // Camera
 
-	point3 lookfrom(278, 278, -800);
-	point3 lookat(278, 278, 0);
-	vec3 vup(0, 1, 0);
-	auto dist_to_focus = 10.0;
-	auto aperture = 0.0;
-	auto vfov = 40.0;
-	auto time0 = 0.0;
-	auto time1 = 1.0;
+    point3 lookfrom(278, 278, -800);
+    point3 lookat(278, 278, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.0;
+    auto vfov = 40.0;
+    auto time0 = 0.0;
+    auto time1 = 1.0;
 
-	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, time0, time1);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, time0, time1);
 
-	// Render
+    // Render
 
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-	std::cerr << "#width,threads,elapsed_seconds\n";
-	std::cerr << image_width << ',' << thread_count << ',';
-	auto start_time = steady_clock::now();
-	std::vector<color> image_buffer(image_width * image_height);
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cerr << "#width,threads,elapsed_seconds\n";
+    std::cerr << image_width << ',' << thread_count << ',';
+    auto start_time = steady_clock::now();
+    std::vector<color> image_buffer(image_width * image_height);
 	  
 #pragma omp parallel for
-	for (int j = image_height-1; j >= 0; --j) {
-		for (int i = 0; i < image_width; ++i) {
-			color pixel_color(0,0,0);
-			for (int s = 0; s < samples_per_pixel; ++s) {
-				auto u = (i + random_double()) / (image_width-1);
-				auto v = (j + random_double()) / (image_height-1);
-				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, background, world, lights, max_depth);
-			}
-			image_buffer[(image_height-1-j) * image_height + i] = pixel_color;
-		}
+    for (int j = image_height-1; j >= 0; --j) {
+	for (int i = 0; i < image_width; ++i) {
+	    color pixel_color(0,0,0);
+	    for (int s = 0; s < samples_per_pixel; ++s) {
+		auto u = (i + random_double()) / (image_width-1);
+		auto v = (j + random_double()) / (image_height-1);
+		ray r = cam.get_ray(u, v);
+		pixel_color += ray_color(r, background, world, lights, max_depth);
+	    }
+	    image_buffer[(image_height-1-j) * image_height + i] = pixel_color;
 	}
-	auto end_time = steady_clock::now();
-	auto elapsed_time = end_time - start_time;
-	auto elapsed_seconds = duration_cast<seconds>(elapsed_time).count();
-	// Write image to file
-	for (auto c: image_buffer) {
-		write_color(std::cout, c, samples_per_pixel);
-	}
-	std::cerr << elapsed_seconds << '\n';
+    }
+    auto end_time = steady_clock::now();
+    auto elapsed_time = end_time - start_time;
+    auto elapsed_seconds = duration_cast<seconds>(elapsed_time).count();
+    // Write image to file
+    for (auto c: image_buffer) {
+	write_color(std::cout, c, samples_per_pixel);
+    }
+    std::cerr << elapsed_seconds << '\n';
 }
